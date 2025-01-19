22769b6e51dcef7e814caf4be4d5fdc29b69b01f
ychik <ychik1997@yandex.ru>
2025-01-19 17:00:59 +0300

    Добавлена папка nuxt-primevue-project в репозиторий

---
+++ nuxt-primevue-project/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <main>
+    <Body />
+  </main>
+  <Footer />
+</template>
+
+<script setup>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+import Body from '~/components/Body.vue';
+</script>
+
+<style scoped>
+main {
+  min-height: 80vh;
+}
+</style>
